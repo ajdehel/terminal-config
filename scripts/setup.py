3eadd56b00f1d9aebab931c230482afc4e7e2a21
andrew dehel <dehel.andrew@gmail.com>
2020-03-30 13:09:42 -0500

    Add forgotten changes to startup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -13,7 +13,7 @@ for each in [".bashrc", ".bash_aliases", ".inputrc", ".vimrc", ".ssh/config", ".
 		os.remove(f"{home}/{each}")
 	except FileNotFoundError:
 		pass
-#create and link conf dir
+#create TERMCONF_PATH env variable
 os.putenv(TERMCONF_ENV, termconf_path)
 
 #link conf files in conf dir
@@ -26,11 +26,11 @@ os.symlink(f"{termconf_path}/bash/home/bash_aliases", f"{home}/.bash_aliases")
 os.symlink(f"{termconf_path}/bash/home/inputrc", f"{home}/.inputrc")
 
 # vim
-os.symlink(f"{termconf_path}/vim/vimrc", f"{home}/.vimrc")
+os.symlink(f"{termconf_path}/vim/home/vimrc", f"{home}/.vimrc")
 
 # git
-os.symlink(f"{termconf_path}/git/gitconfig", f"{home}/.gitconfig")
+os.symlink(f"{termconf_path}/git/home/gitconfig", f"{home}/.gitconfig")
 
 # link to ssh config
-os.symlink(f"{termconf_path}/ssh/config", f"{home}/.ssh/config")
+os.symlink(f"{termconf_path}/ssh/home/config", f"{home}/.ssh/config")
 
