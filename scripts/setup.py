aeaafc86fe97de1483ea9694fe53b1423548aff9
andrew dehel <dehel.andrew@gmail.com>
2020-05-07 11:58:05 -0500

    Make setup.py dynamically create links from $HOME

--- scripts/setup.py
+++ scripts/setup.py
@@ -1,32 +1,46 @@
 #!/usr/bin/python3
 
 import os
+import pathlib
 
-home = os.getenv("HOME")
-user = os.getenv("USER")
-termconf_path = f"{home}/.{user}"
-TERMCONF_ENV = "TERMCONF_PATH"
+home = pathlib.Path.home()
+user = home.owner()
+termconf_path = home.joinpath(f".{user}")
 
-#remove any potentially pre-existing configs
-for each in [".bashrc", ".bash_aliases", ".inputrc", ".vimrc", ".gitconfig"]:
-	try:
-		os.remove(f"{home}/{each}")
-	except FileNotFoundError:
-		pass
-#create TERMCONF_PATH env variable
-os.putenv(TERMCONF_ENV, termconf_path)
+def find_config_files(path):
+    config_dirs = path.rglob("home/")
+    config_paths = list()
+    for path in config_dirs:
+        config_paths.extend([config.resolve() for config in path.iterdir()])
+    return config_paths
 
-#link conf files in conf dir
+def create_link_dict(config_paths):
+    link_dict = dict()
+    for link_target in config_paths:
+        link_path   = home.joinpath(f".{link_target.name}")
+        link_dict[link_path] = link_target
+    return link_dict
 
-# bash
-os.symlink(f"{termconf_path}/bash/home/bashrc", f"{home}/.bashrc")
-os.symlink(f"{termconf_path}/bash/home/bash_aliases", f"{home}/.bash_aliases")
+def clear_symlinks(link_list):
+    for link_path in link_list:
+        try:
+            link_path.unlink()
+            print(f"Removed '{link_path}'.")
+        except FileNotFoundError:
+            pass
 
-# input
-os.symlink(f"{termconf_path}/bash/home/inputrc", f"{home}/.inputrc")
+def create_symlinks(link_dict):
+    """Take link_dict and create a key => value link for each key"""
+    clear_symlinks(link_dict.keys())
+    for link_path, target in link_dict.items():
+        print(f"Link '{str(link_path:<20}' => '{target}'")
+        link_path.symlink_to(target)
 
-# vim
-os.symlink(f"{termconf_path}/vim/home/vimrc", f"{home}/.vimrc")
+def main():
+    config_paths = find_config_files(termconf_path)
+    link_dict    = create_link_dict(config_paths)
+    create_symlinks(link_dict)
 
-# git
-os.symlink(f"{termconf_path}/git/home/gitconfig", f"{home}/.gitconfig")
+if "__main__" == __name__:
+    main()
+
