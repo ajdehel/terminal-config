14ea1433838b5eb8cc91cefe3baa8cd0d36cbc5e
andrew dehel <dehel.andrew@gmail.com>
2021-05-21 19:53:45 -0500

    Update PROMPT_COMMAND for success/failure PS1

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,4 +1,15 @@
-PS1=">>>"
+function prompt_command {
+  old_exit=$?
+  if [[ 0 -eq $old_exit ]]; then
+    PS1="$>"
+  else
+    PS1="!>"
+  fi
+  $TERMCONF_PATH/scripts/prompt.py
+  return $old_exit
+}
+
 PS2="..."
+
 XKB_COMMAND='capsoption;'
-PROMPT_COMMAND='$TERMCONF_PATH/scripts/prompt.py'
+PROMPT_COMMAND=prompt_command
